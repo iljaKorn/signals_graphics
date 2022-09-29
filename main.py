02977ec5670976755fb2b29315d901c12e55c74d
iljaKorn <93839868+iljaKorn@users.noreply.github.com>
2022-09-29 22:27:43 +0300

    add digital signal and spectrum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import numpy
 import numpy as np
 from scipy.fft import rfft, rfftfreq
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 
 SAMPLE_RATE = 200  # Гц
 DURATION = 5  # Секунды
@@ -44,14 +42,7 @@ def spectrum_sig(sin_sig1, sin_sig2, sin_sig3, sin_sig4):
     plt.show()
 
 
-def graphic_sig():
-    t = np.linspace(0, DURATION, SAMPLE_RATE * DURATION, endpoint=False)
-
-    sig_with_w1 = get_y_array(1, t)
-    sig_with_w2 = get_y_array(2, t)
-    sig_with_w3 = get_y_array(4, t)
-    sig_with_w4 = get_y_array(8, t)
-
+def graphic_harmonic_sig(t, sig_with_w1, sig_with_w2, sig_with_w3, sig_with_w4):
     figure, axis = plt.subplots(2, 2)
 
     axis[0, 0].plot(t, sig_with_w1)
@@ -75,8 +66,54 @@ def graphic_sig():
     spectrum_sig(sig_with_w1, sig_with_w2, sig_with_w3, sig_with_w4)
 
 
+def get_digital_sig(sig):
+    new_sig = np.sign(sig)
+    for i in range(len(new_sig)):
+        if new_sig[i] < 0:
+            new_sig[i] = 0
+    return new_sig
+
+
+def graphic_digital_sig(t, sig_with_w1, sig_with_w2, sig_with_w3, sig_with_w4):
+    sig_w1 = get_digital_sig(sig_with_w1)
+    sig_w2 = get_digital_sig(sig_with_w2)
+    sig_w3 = get_digital_sig(sig_with_w3)
+    sig_w4 = get_digital_sig(sig_with_w4)
+
+    figure, axis = plt.subplots(2, 2)
+
+    axis[0, 0].plot(t, sig_w1)
+    axis[0, 0].set_title("Signal with frequency 1")
+    axis[0, 0].grid()
+
+    axis[1, 0].plot(t, sig_w2)
+    axis[1, 0].set_title("Signal with frequency 2")
+    axis[1, 0].grid()
+
+    axis[0, 1].plot(t, sig_w3)
+    axis[0, 1].set_title("Signal with frequency 4")
+    axis[0, 1].grid()
+
+    axis[1, 1].plot(t, sig_w4)
+    axis[1, 1].set_title("Signal with frequency 8")
+    axis[1, 1].grid()
+
+    plt.show()
+
+    spectrum_sig(sig_w1, sig_w2, sig_w3, sig_w4)
+
+
 def main():
-    graphic_sig()
+    t = np.linspace(0, DURATION, SAMPLE_RATE * DURATION, endpoint=False)
+
+    sig_with_w1 = get_y_array(1, t)
+    sig_with_w2 = get_y_array(2, t)
+    sig_with_w3 = get_y_array(4, t)
+    sig_with_w4 = get_y_array(8, t)
+
+    graphic_harmonic_sig(t, sig_with_w1, sig_with_w2, sig_with_w3, sig_with_w4)
+
+    graphic_digital_sig(t, sig_with_w1, sig_with_w2, sig_with_w3, sig_with_w4)
 
 
 if __name__ == '__main__':
